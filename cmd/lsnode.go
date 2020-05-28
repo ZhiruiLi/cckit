015a28dfb0f3c0d5cbd0e663f4c0dc48f91a6975
zhiruili <zr.public@outlook.com>
2020-05-28 22:49:44 +0800

    update

--- cmd/lsnode.go
+++ cmd/lsnode.go
@@ -5,6 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
+
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
@@ -17,22 +18,39 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+
+	"encoding/json"
 
 	"github.com/spf13/cobra"
 )
 
+var cutHead uint32
+
 // lsnodeCmd represents the lsnode command
 var lsnodeCmd = &cobra.Command{
 	Use:   "lsnode",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List nodes of prefab files",
+	Long:  `List nodes of prefab files.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("lsnode called")
+		for _, filename := range args {
+			var nodes []map[string]interface{}
+			log.Println("got: ", filename)
+			data, err := ioutil.ReadFile(filename)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			if err := json.Unmarshal(data, &nodes); err != nil {
+				log.Fatalln(err)
+			}
+			for _, node := range nodes {
+				for k, v := range node {
+					log.Println(k, v)
+				}
+			}
+		}
 	},
 }
 
@@ -44,6 +62,7 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// lsnodeCmd.PersistentFlags().String("foo", "", "A help for foo")
+	lsnodeCmd.PersistentFlags().Uint32VarP(&cutHead, "cuthead", "", 1, "Remove N level from root")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
