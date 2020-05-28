f90cb559f148394f48b7803405b0f0068ebf2aba
zhiruili <zr.public@outlook.com>
2020-05-28 22:33:07 +0800

    update

--- cmd/lsnode.go
+++ cmd/lsnode.go
@@ -21,10 +21,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// listpCmd represents the listp command
-var listpCmd = &cobra.Command{
-	Use:   "listp",
-	Short: "List all nodes of a prefab file",
+// lsnodeCmd represents the lsnode command
+var lsnodeCmd = &cobra.Command{
+	Use:   "lsnode",
+	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -32,20 +32,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("listp called")
+		fmt.Println("lsnode called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(listpCmd)
+	rootCmd.AddCommand(lsnodeCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// listpCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// lsnodeCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// listpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// lsnodeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
